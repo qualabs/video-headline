6401ea8e8497e864ec19f52e7925440a5b73918a
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2023-10-30 17:23:23 -0300

    Refactor code

--- api/lambda/list_channels.py
+++ api/lambda/list_channels.py
@@ -13,35 +13,64 @@ def json_serial(obj):
 def lambda_handler(event, context):
     path_parameters = event.get('pathParameters', {})
     channel_id = path_parameters.get('id', None)
-
+    response = {}
+    
     if channel_id:
         try:
             response = media_tailor.describe_channel(ChannelName=channel_id)
             channel_data = response.get('Channel', {})
-            channel['CreationTime'] = json_serial(channel['CreationTime'])
-            channel['LastModifiedTime'] = json_serial(channel['LastModifiedTime'])            
-            return {
-                'statusCode': 200,
-                'body': json.dumps(channel_data, default=json_serial)
-            }
+            channel_data['CreationTime'] = json_serial(channel_data['CreationTime'])
+            channel_data['LastModifiedTime'] = json_serial(channel_data['LastModifiedTime']) 
+            if channel_data :
+                response = {
+                    'status_code': 200,
+                    'body' : { 'messsage' : 'Created succesfuly',
+                                'data' : json.dump(channel_data, default=json_serial) 
+                        
+                    }
+                }
+                
+                
+            else :
+                response = { 'status_code': 404,
+                            'body' : {
+                                'message': 'Channel not found',
+                                'data' : []
+                                }
+                            }
+                raise Exception(response)
+
+                
         except media_tailor.exceptions.ClientError as e:
-            return {
-                'statusCode': 404,
-                'body': json.dumps({'message': 'Channel not found'})
-            }
+                response = { 'status_code': 404,
+                            'body' : {
+                                'message': 'Channel not found',
+                                'data' : []
+                                }
+                            }
+                raise Exception(response)
     else:
         try:
             response = media_tailor.list_channels()
             channels_data = response.get('Items', [])
             channels_data = list(map(lambda channel: {**channel, 'CreationTime': json_serial(channel['CreationTime']), 'LastModifiedTime' : json_serial(channel['LastModifiedTime'])}, channels_data))
-
-
-            return {
-                'statusCode': 200,
-                'body': json.dumps(channels_data, default=json_serial)
+            
+            response = {
+                'status_code' : 200,
+                'body' : {
+                    'message' : 'Channel retrieved succesfuly',
+                    'data' : json.dumps(channels_data, default=json_serial)
+                }
             }
+
         except media_tailor.exceptions.ClientError as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'message': 'Internal Server Error'})
+            response = {
+                'status_code' : 500,
+                'body' : {
+                    'message' : 'Internal Server Error',
+                    'data' : []
+                }
             }
+
+
+        return response
--- api/lambda/list_tables.py
+++ api/lambda/list_tables.py
@@ -1,52 +1,76 @@
 import json
 import boto3
-from boto3.dynamodb.conditions import Key
+from datetime import datetime
+from datetime import date
 
+media_tailor = boto3.client('mediatailor')
+
+def json_serial(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
 
 def lambda_handler(event, context):
     path_parameters = event.get('pathParameters', {})
-    id = path_parameters.get('id')
-    dynamodb = boto3.resource('dynamodb')
-    dynamo_table = path_parameters.get('table')
-    table = dynamodb.Table(dynamo_table) 
+    channel_id = path_parameters.get('id', None)
     response = {}
-
-    if id is not None:
+    
+    if channel_id:
         try:
-            table_response = table.query(
-                KeyConditionExpression=Key('ScheduleId').eq(id)
-            )
-            items = table_response.get('Items', [])  
-            if items:
-                response["status_code"] = 200
-                body = { "message" : "Item found succesfuly",
-                        "data" : json.dumps(items)}
-                response ["body"] = body
+            response = media_tailor.describe_channel(ChannelName=channel_id)
+            channel_data = response.get('Channel', {})
+            channel_data['CreationTime'] = json_serial(channel_data['CreationTime'])
+            channel_data['LastModifiedTime'] = json_serial(channel_data['LastModifiedTime']) 
+            if channel_data :
+                response = {
+                    'status_code': 200,
+                    'body' : { 'messsage' : 'Created succesfuly',
+                                'data' : json.dump(channel_data, default=json_serial) 
+                        
+                    }
+                }
                 
-            else:
-                response["status_code"] = 404 
-                body = {"message" : "Item not found",
-                        "data" : []}
-                response["body"] = body
                 
-        except Exception as e:
-            response["status_code"] = 500
-            body  = {"message" : 'Internal server error',
-                    "data" : []}
+            else :
+                response = { 'status_code': 404,
+                            'body' : {
+                                'message': 'Channel not found',
+                                'data' : []
+                                }
+                            }
+                raise Exception(response)
+
+                
+        except media_tailor.exceptions.ClientError as e:
+                response = { 'status_code': 404,
+                            'body' : {
+                                'message': 'Channel not found',
+                                'data' : []
+                                }
+                            }
+                raise Exception(response)
     else:
         try:
-            table_response = table.scan()
-            items = table_response.get('Items', [])
-            response["status_code"] = 200
-            body = {"message" : "Items found succesfuly",
-                   "data" : json.dumps(items)}
-            response["body"] = body
+            response = media_tailor.list_channels()
+            channels_data = response.get('Items', [])
+            channels_data = list(map(lambda channel: {**channel, 'CreationTime': json_serial(channel['CreationTime']), 'LastModifiedTime' : json_serial(channel['LastModifiedTime'])}, channels_data))
+            
+            response = {
+                'status_code' : 200,
+                'body' : {
+                    'message' : 'Channel retrieved succesfuly',
+                    'data' : json.dumps(channels_data, default=json_serial)
+                }
+            }
 
-        except Exception as e:
-            status_code = 500
-            body = {"message" : "Internal server error",
-                    "data" : [] }
-            response["body"] = body
+        except media_tailor.exceptions.ClientError as e:
+            response = {
+                'status_code' : 500,
+                'body' : {
+                    'message' : 'Internal Server Error',
+                    'data' : []
+                }
+            }
 
 
-    return response
+        return response
