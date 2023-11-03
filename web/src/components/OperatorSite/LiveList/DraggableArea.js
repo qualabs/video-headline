3a97cac5f72f4b045584bd4a7236669d9aa09210
Felipe Young <felipey@qualabs.com>
2023-11-03 16:38:30 -0300

    adding css and extra functionalities to dropable

--- web/src/components/OperatorSite/LiveList/DraggableArea.js
+++ web/src/components/OperatorSite/LiveList/DraggableArea.js
@@ -1,31 +1,56 @@
 import React, {Component} from 'react'
-
 import './index.css'
 import Dragula from 'react-dragula'
 
 class DraggableArea extends Component {
-
   dragulaDecorator = (componentBackingInstance) => {
     if (componentBackingInstance) {
-      let options = { }
-      Dragula([componentBackingInstance], options)
+      let options = {
+        revertOnSpill: true
+      }
+
+      Dragula(
+        [
+          document.getElementById('assets'),
+          document.getElementById('loop_order')
+        ],
+        options
+      ).on('drop', () => console.log('Item dropped'))
     }
   };
 
   render () {
+    const assets = [
+      {title: 'Ad Available', info: 'Variable Length'},
+      {title: 'Ad Available 2', info: 'Variable Length 2'}
+    ]
+
+    const loopOrder = [{title: 'Qualabs Intro', info: '1 Hour'}]
+
     return (
-      <div className='container' ref={this.dragulaDecorator}>
-        <div>Swap me around</div>
-        <div>Swap her around</div>
-        <div>Swap him around</div>
-        <div>Swap them around</div>
-        <div>Swap us around</div>
-        <div>Swap things around</div>
-        <div>Swap everything around</div>
+      <div className='container'>
+        <div id='assets' className='assets' ref={this.dragulaDecorator}>
+          {assets.map((item) => (
+            <DraggableItem key={item.title} {...item} />
+          ))}
+        </div>
+        <div id='loop_order' className='loop_order' ref={this.dragulaDecorator}>
+          {loopOrder.map((item) => (
+            <DraggableItem key={item.title} {...item} />
+          ))}
+        </div>
       </div>
     )
   }
 }
 
+const DraggableItem = ({title, info}) => (
+  <div className='draggable-item'>
+    <div>
+      <div className='item-title'>{title}</div>
+      <div className='item-info'>{info}</div>
+    </div>
+  </div>
+)
 
 export default DraggableArea
