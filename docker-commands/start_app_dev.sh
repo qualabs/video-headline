8602135db88b7c9bfecdf488e8ae56a8eacf9e0e
Joselen Cecilia <joselen.cecilia@qualabs.com>
2023-11-22 15:49:16 -0300

    add script to create super user

---
+++ docker-commands/start_app_dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Migrating and collecting statics"
+python manage.py migrate --noinput # Apply database migrations
+
+echo "Executing create_super_user.sh script"
+
+# Set superuser credentials
+username="admin"
+email="admin@admin.com"
+password="12345678"
+
+echo "Creating superuser"
+
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+try:
+    User.objects.create_superuser('$username', '$email', '$password')
+    print('Superuser was created successfully')
+except Exception as e:
+    print('Error when creating superuser:', str(e))
+" | python manage.py shell
+
+
+echo "Run the Django development server"
+if [ "$1" == "plus" ]; then
+  python manage.py runserver_plus 0:80
+else
+  python manage.py runserver 0:80
+fi
+
