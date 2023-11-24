14622ee7ca66733dc774ae553f58777564c83645
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2023-11-24 15:55:59 -0300

    Fix proxy

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(proxy('/api', {target: 'http://localhost:8010/'}))
-  app.use(proxy('/admin', {target: 'http://localhost:8010/'}))
-  app.use(proxy('/player', {target: 'http://localhost:8010/'}))
+  app.use(proxy('/api', {target: 'http://video-hub/'}))
+  app.use(proxy('/admin', {target: 'http://video-hub/'}))
+  app.use(proxy('/player', {target: 'http://video-hub/'}))
 }
