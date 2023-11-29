2b26d694c8f0677890f4f6c5c3781984443a31ce
choche <joselen.cecilia@qualabs.com>
2023-11-29 14:39:25 -0300

    delete quotes in id

--- infrastructure/create_super_user.sh
+++ infrastructure/create_super_user.sh
@@ -2,13 +2,12 @@
 username="$1"
 email="$2"
 password="$3"
-organization_id= 1 
 
 echo "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 try:
-    User.objects.create_superuser(username='$username', email='$email', password='$password',organization_id='$organization_id')
+    User.objects.create_superuser(username='$username', email='$email', password='$password',organization_id=1)
     print('Superuser was created successfully: $username email: $email')
 except Exception as e:
     print('Error when creating superuser:', str(e))
