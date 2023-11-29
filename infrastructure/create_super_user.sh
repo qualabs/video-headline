2ea74eea5c5964d273e4848551a02b307cee235c
JoselenCeciliaQ <146980715+JoselenCeciliaQ@users.noreply.github.com>
2023-11-29 14:51:33 -0300

    Update infrastructure/create_super_user.sh
    Co-authored-by: Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>

--- infrastructure/create_super_user.sh
+++ infrastructure/create_super_user.sh
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 try:
     User.objects.create_superuser(username='$username', email='$email', password='$password',organization_id=1)
-    print('Superuser was created successfully: $username email: $email')
+    print('Superuser was created successfully with name $username and email $email')
 except Exception as e:
     print('Error when creating superuser:', str(e))
 " | python manage.py shell
